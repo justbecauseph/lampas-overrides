9ece515a3e8f2ae0504fa8a07cebb65086bc92b5
Mark Jayson Fuentes <awkwardusername@gmail.com>
2026-05-29 00:01:49 +0800

    chore: revert logging

--- src/main/java/town/lampas/overrides/PlayerActivityListener.java
+++ src/main/java/town/lampas/overrides/PlayerActivityListener.java
@@ -49,7 +49,6 @@ public class PlayerActivityListener {
         // UUID is alphanumeric with hyphens, and username is alphanumeric with underscores.
         // Thus, formatting is completely safe and doesn't require complex escaping.
         String json = String.format("{\"uuid\":\"%s\",\"username\":\"%s\",\"event\":\"%s\"}", uuid, username, eventType);
-        LOGGER.info("Payload for player {} event: {}", username, json);
 
         try {
             HttpRequest request = HttpRequest.newBuilder()
